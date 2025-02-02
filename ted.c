6e77a1b93064b4b6cebb075c44a4595c96d6d863
Nino Carrillo <kk4hej@gmail.com>
2025-02-02 14:01:09 -0500

    add bit clock feedback

--- ted.c
+++ ted.c
@@ -12,6 +12,22 @@ void InitSlice2(Data_Slicer_struct *slicer, float sample_rate, float symbol_rate
 	slicer->AccumulatorBitWidth = (sizeof(long int) * CHAR_BIT) - 1;
 	slicer->DataAccumulator = 0;
 }
+int ZDetect(float old_y, float new_y) {
+    int crossed = 0;
+    // Detect zero crossing.
+    if (old_y > 0) {
+        // Last sample was positive.
+        if (new_y <= 0) {
+            crossed = 1;
+        }
+    } else if (old_y <= 0) {
+        // Last sample was negative.
+        if (new_y > 0) {
+            crossed = 1;
+        }
+    }
+    return crossed;
+}
 
 long int Slice2(Data_Slicer_struct *slicer, float sample) {
     long int result = -1;
@@ -36,6 +52,10 @@ long int Slice2(Data_Slicer_struct *slicer, float sample) {
             slicer->MatchDCD = 120;
         }
     }
+	if (ZDetect(slicer->LastSample, sample)) {
+		slicer->Clock *= slicer->LockRate;
+	}
+	slicer->LastSample = sample;
     return result;
 }
 
